50492bb6df6cdc49243b1d3e505b89ab9e852e1d
wyubin <yubin0611@gmail.com>
2018-09-05 11:34:46 +0800

    add filelist, revise css update

--- myform.js
+++ myform.js
@@ -1,11 +1,13 @@
+"use strict"
 module.id='myform';
 // general object extend
 require('../../lib/stdlib.HTMLElement.js');
 require('../../lib/stdlib.HTMLFormElement.js');
 require('../../lib/stdlib.Navigator.js');
 require('../../lib/stdlib.CSSStyleSheet.js');
+require('../../lib/stdlib.FileList.js');
 var extend = require('util')._extend;
-var events_reg = require('../../lib/events_reg.js').events_reg;
+var events_reg = require('../../lib/events_reg.js');
 
 /**
 * create(new) form view without jquery with simple attr and reset, submit functions.
@@ -20,40 +22,56 @@ var myform = function (dom,args){
 	},args);
 	// initial event register
 	this.e_reg = new events_reg(dom);
-	this.validators = {};
+	this.files = {};
 	this.init();
 };
-exports.myform = myform;
+module.exports = myform;
 /**
 * init process myform_view, submit trigger need pass validate_handler and cb_submit
 */
 myform.prototype.init = function(){
-	var self= this,tmp;
+	var self= this;
 	this.init_view();
 	// function support prepare
 	this.args.events.map(function(v){self.e_reg.events[v]={}});
 	// init event register, submit init function
 	this.e_reg.events.submit = function(e){
+		e.preventDefault();
 		// check browser type and response
 		if(navigator.browser_version()[0].toLowerCase() == 'safari'){
 			if(e.currentTarget.safari_validity(e)){
 				self.submit_callback(e);
 			}
 		}else{self.submit_callback(e);}
-		e.preventDefault();
 	};
-	// change event
+	// input event
 	this.e_reg.events.input = function(e){
 		e.currentTarget.validate_handler(e);
 	}
-	// create self css
-	tmp = document.querySelector('head>style[title='+this.args.custom_class+']');
-	if(tmp==null){
-		tmp = document.createElement('style');
-		tmp.setAttribute('title',this.args.custom_class);
-		document.querySelector('head').appendChild(tmp);
+	/* input file change to preload, single file
+	var f_r = new FileReader(),f_n;
+	f_r.onload = function(e){self.files[f_n] = e.target.result;}
+	this.e_reg.events.change['input[type=file]'] = function(e){
+		var t_dom = e.target;
+		f_n = t_dom.name;
+		if(t_dom.files.length){
+			f_r.readAsDataURL(t_dom.files[0]);
+		}else{
+			self.files[f_n] = '';
+		}
+	}*/
+	// input file change to preload, multiple files
+	this.e_reg.events.change['input[type=file]'] = function(e){
+		e.target.files.readPromise().then(function(data){
+			self.files[e.target.name] = data;
+		});
 	}
-	this.css = tmp.sheet;
+	// create self css if null
+	if(!this.args.css){
+		var t_dom = document.createElement('style');
+		document.head.appendChild(t_dom);
+		this.args.css = t_dom.sheet;
+	}
 }
 /**
 * init view constructs, add base form structure and reset and submit function
@@ -64,7 +82,7 @@ myform.prototype.init_view = function(){
 		{name:'div',attr:{class:'form_input'}},
 		{name:'div',attr:{class:'form_ctrl'},
 			child:['reset','submit'].map(function(v){
-				return {name:'input',attr:{name:v,type:v}};
+				return {name:'input',attr:{type:v}};
 			})
 		}
 	]);
@@ -72,8 +90,10 @@ myform.prototype.init_view = function(){
 	['form_input','form_ctrl'].map(function(v){
 		self.doms[v] = self.doms.body.querySelector('div.'+v);
 	});
-	this.doms.body.classList.add('myform');
-	this.doms.body.classList.add(this.args.custom_class);
+	// add classes
+	'myform hide'.split(/\s+/).concat([this.args.custom_class]).map(function(v){
+		self.doms.body.classList.add(v);
+	});
 }
 /**
 * create multiple inputs in form on the fly.
@@ -81,22 +101,33 @@ myform.prototype.init_view = function(){
 * @returns {Object} this
 */
 myform.prototype.add_inputs = function(dom_json){
-	var t_dom_j;
-	var doms_json = dom_json.map(function(dom_j){
-		if(dom_j.no_label){
+	var t_dom_j,t_label,hide_inds=[];
+	var doms_json = dom_json.map(function(dom_j,ind){
+		t_label=dom_j.label || dom_j.attr.name;
+		if(dom_j.no_label || !t_label){
 			t_dom_j = dom_j;
 		}else{
-			t_dom_j = {name:'div',attr:{class: dom_j.hide ? 'field_hide':'field'},
-				child:[dom_j,{name:'label',child:[dom_j.attr.name]}]
+			t_dom_j = {name:'div',attr:{class:'field'},
+				child:[dom_j,{name:'label',child:[t_label]}]
 			};
+		}
+		if(dom_j.hide){
+			hide_inds.push(ind);
 		}
 		return t_dom_j;
 	});
+	var self = this;
 	this.doms.form_input.append_by_array(doms_json);
+	// display now
+	this.doms.body.classList.remove('hide');
 	// css update
 	this.css_update();
 	// register event each time
 	this.e_reg.register();
+	// hide fields
+	hide_inds.map(function(v){
+		self.doms.form_input.children[v].classList.add('hide');
+	});
 	return this;
 }
 /**
@@ -106,6 +137,10 @@ myform.prototype.add_inputs = function(dom_json){
 myform.prototype.css_update = function(){
 	var a_doms,self=this,max_width={},
 		selectors={'label':'div.field>label','element':'div.field>*:nth-child(1)'};
+	// remove previuos css
+	this.args.css.clean([' div.field > label','',' div.field > :nth-child(1)'].map(function(v){
+		return 'form.'+self.args.custom_class+v;
+	}));
 	// count width of label and element
 	Object.keys(selectors).map(function(v){
 		a_doms = Array.prototype.slice.call(self.doms.body.querySelectorAll(selectors[v]));
@@ -113,17 +148,16 @@ myform.prototype.css_update = function(){
 			return v_1.getBoundingClientRect().width;
 		}));
 	});
-	var form_width = this.doms.body.getBoundingClientRect().width;
 	// label width
-	this.css.css_update('form.'+this.args.custom_class+' div.field>label',{width:max_width.label});
+	this.args.css.css_update('form.'+this.args.custom_class+' div.field > label',{width:max_width.label+'px'});
 	// form min-width=label_width+100, max-width=label_width+input_width
-	this.css.css_update('form.'+this.args.custom_class,{
-		'min-width':max_width.label+100,
-		'max-width':max_width.label+max_width.element+30
+	this.args.css.css_update('form.'+this.args.custom_class,{
+		'max-width':'calc('+(max_width.label+max_width.element)+'px + 2em)'
 	});
+	var form_width = this.doms.body.getBoundingClientRect().width;
 	// input max-width = form_width-label_width
-	this.css.css_update('form.'+this.args.custom_class+' div.field>*:nth-child(1)',{
-		'max-width':form_width-max_width.label-40
+	this.args.css.css_update('form.'+this.args.custom_class+' div.field > :nth-child(1)',{
+		'max-width':'calc('+(form_width-max_width.label)+'px - 4em)'
 	});
 	return this;
 }
